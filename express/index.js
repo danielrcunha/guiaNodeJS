93cc94ca1317ee58657822efee95c4c3add1e212
danielrcunha <74882067+danielrcunha@users.noreply.github.com>
2022-05-31 21:42:13 -0300

    commit
    commit

--- express/index.js
+++ express/index.js
@@ -9,20 +9,41 @@ app.get("/", function (req, res) {
     res.send("<h1>Bem vindo ao guia do programador!</h1>");
 });
 
-app.get("/blog", function (req, res) {
+/*parametro não obrigatorio :artigo?*/
+app.get("/blog/:artigo?", function (req, res) {
+
+    var artigo = req.params.artigo;
+    if (artigo) {
+        res.send("<h2>Artigo: " + artigo + " </h2>");
+    } else {
+        res.send("<h2>Bem-vindo ao Blog!</h2>");
+    }
+
     res.send("<h1>Você está no Blog, seja bem-vindo!</h1>");
 });
 
+/*utilizando query param*/
 app.get("/canal/youtube", function (req, res) {
-    res.send("<h1>Bem-vindo ao meu canal!</h1>");
+    var canal = req.query["canal"];
+
+    if (canal) {
+        res.send(canal);
+    } else {
+        res.send("nenhum canal fornecido");
+    }
+
+    res.send(canal);
+    //navegador :: http://localhost:4000/canal/youtube?canal=valores
+    //res.send("<h1>Bem-vindo ao meu canal!</h1>");
 });
 
-app.get("/Ola/:nome", function (req, res) {
+app.get("/Ola/:nome/:empresa", function (req, res) {
     //req :: dados enviados pelo usuario
     //res :: resposta que vai ser enviada para o usuario
 
     var nome = req.params.nome;
-    res.send("<h1>Oi " + nome + "</h1>");
+    var empresa = req.params.empresa;
+    res.send("<h1>Oi " + nome + " da " + empresa + "</h1>");
 });
 
 app.listen(4000, function (erro) {
