33b148fe377cef6b32f417d12e19f5b8269430c8
danielrcunha <74882067+danielrcunha@users.noreply.github.com>
2022-05-31 21:02:00 -0300

    commit
    commit

--- express/index.js
+++ express/index.js
@@ -4,6 +4,27 @@ const express = require("express");
 /*iniciando o express*/
 const app = express();
 
+/*criando a rota inicial*/
+app.get("/", function (req, res) {
+    res.send("<h1>Bem vindo ao guia do programador!</h1>");
+});
+
+app.get("/blog", function (req, res) {
+    res.send("<h1>Você está no Blog, seja bem-vindo!</h1>");
+});
+
+app.get("/canal/youtube", function (req, res) {
+    res.send("<h1>Bem-vindo ao meu canal!</h1>");
+});
+
+app.get("/Ola/:nome", function (req, res) {
+    //req :: dados enviados pelo usuario
+    //res :: resposta que vai ser enviada para o usuario
+
+    var nome = req.params.nome;
+    res.send("<h1>Oi " + nome + "</h1>");
+});
+
 app.listen(4000, function (erro) {
     if (erro) {
         console.log("Ocorreu um erro");
